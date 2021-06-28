0e26c9005a34a043565e0f1c2236e61e19059ed5
jhon <jhonatan.arguello@gmail.com>
2021-06-28 01:09:40 -0300

    Finalizaçao do NLW

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -13,6 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -54,7 +56,7 @@ class _LoginPageState extends State<LoginPage> {
                           right: size.width * 0.185),
                       child: Text("Organize seus boletos em um só lugar",
                           textAlign: TextAlign.center,
-                          style: TextStyles.titleHome),
+                          style: TextStyles.titleRegular),
                     ),
                     Padding(
                       padding: EdgeInsets.only(
@@ -63,7 +65,7 @@ class _LoginPageState extends State<LoginPage> {
                           left: size.width * 0.15,
                           right: size.width * 0.15),
                       child: SocialLoginButton(onTap: () {
-                        print('clicou');
+                        controller.googleSignIn(context);
                       }),
                     )
                   ],
